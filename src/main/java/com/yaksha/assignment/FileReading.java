765c3cc15e89b025222e7e507298508e26062e6a
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-27 18:48:55 +0530

    Update FileReading.java

--- src/main/java/com/yaksha/assignment/FileReading.java
+++ src/main/java/com/yaksha/assignment/FileReading.java
@@ -8,26 +8,9 @@ public class FileReading {
 
 	public static void main(String[] args) {
 		// **Create a file instance**
-		File file = new File("readFile.txt");
 
-		try {
-			// Check if the file exists
-			if (!file.exists()) {
-				System.out.println("File does not exist.");
-				return;
-			}
+		// Check if the file exists
 
-			// **Read the content of the file**
-			Scanner reader = new Scanner(file);
-			System.out.println("Reading content from the file:");
-			while (reader.hasNextLine()) {
-				String line = reader.nextLine();
-				System.out.println(line);
-			}
-			reader.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("An error occurred. File not found.");
-			e.printStackTrace();
-		}
+		// **Read the content of the file**
 	}
 }
